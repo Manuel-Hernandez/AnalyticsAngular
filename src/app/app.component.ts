bdf9626ac90902c20e2e24ac2d493901b2c8b3e3
Manuel Hernandez <jamcv130193@gmail.com>
2019-07-01 21:26:49 -0500

    visualización de graficas de analytics en Angular 6 ejemplo basado en el proyecto de nicobytes

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'codelab-google-analytics';
+
+  constructor(
+    private router: Router
+  ) {
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'SU_ID_DE_SEGUIMIENTO', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
+}
